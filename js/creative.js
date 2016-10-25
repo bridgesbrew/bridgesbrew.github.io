b82ea6611b3e73d9f37fc65eb3323fefe33c3681
Maura Lynch <MauraLynch@Mair.local>
2016-10-24 21:27:40 -0700

    Adding js for Google Analytics events on click

--- js/creative.js
+++ js/creative.js
@@ -45,20 +45,22 @@
         distance: '0px'
     }, 300);
 
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
+    // Initialize and Configure Click Tracking
+	$('#payLink').click(function() {
+		ga('send', 'event', {
+		    eventCategory: 'Pay Link',
+		    eventAction: 'click',
+		    transport: 'beacon'
+		});
+	});
+	$('#phoneCall').click(function() {
+	  ga('send', 'event', 'Phone Call', 'click');
+	});
+	$('#email').click(function() {
+	  ga('send', 'event', 'Email', 'click');
+	});
+	$('#seeMore').click(function() {
+	  ga('send', 'event', 'See More Order Now', 'click');
+	});
 
 })(jQuery); // End of use strict
